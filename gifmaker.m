3aeb2cef63efe10350de377d451ca04b8a0a681c
Jeffery <94628699+Mr-Jeffery@users.noreply.github.com>
2022-12-06 19:21:50 +0800

    Update gifmaker.m
    more comments added

--- gifmaker.m
+++ gifmaker.m
@@ -1,15 +1,12 @@
 clear; 
 clc;
 
-n = 2:2:10;
-nImages = length(n);
+n = 1:1:10;
+nImages = length(n);%calculate frame number
 fig = figure;
 for idx = 1:nImages
-    %
-    %whatever you want to draw
-    %drawnow
-    %xlim([-0.5,3.5]);
-    %ylim([0,1.2]);
+    %fill in whatever you want to draw
+    drawnow
     frame = getframe(fig);
     im{idx} = frame2im(frame);
 end
@@ -18,8 +15,8 @@ filename = 'testAnimated.gif'; % Specify the output file name
 for idx = 1:nImages
     [A,map] = rgb2ind(im{idx},256);
     if idx == 1
-        imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',0.8);
+        imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',0.8);%first frame, used to create file
     else
-        imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',0.8);
+        imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',0.8);%adding more frames
     end
 end
